12ddcc4ac334cc6be3441bc6d0572129a48220cc
Din Muhammad Rezwoan <frezwoan@gmail.com>
2025-06-11 06:54:34 +0600

    test

--- Store Management Application/Program.cs
+++ Store Management Application/Program.cs
@@ -8,9 +8,9 @@ namespace Store_Management_Application
 {
     internal static class Program
     {
-        /// <summary>
+        
         /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
