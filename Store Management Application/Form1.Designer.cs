582f47fef97e0bf553ace903c512d23e499b835b
Soumikdas3210 <soumikdas3210@gmail.com>
2025-06-20 22:51:47 +0600

    Added login

---
+++ Store Management Application/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Store_Management_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            materialMultiLineTextBox1 = new MaterialSkin.Controls.MaterialMultiLineTextBox();
+            materialMultiLineTextBox2 = new MaterialSkin.Controls.MaterialMultiLineTextBox();
+            materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(materialButton1);
+            panel1.Controls.Add(materialMultiLineTextBox2);
+            panel1.Controls.Add(materialLabel2);
+            panel1.Controls.Add(materialMultiLineTextBox1);
+            panel1.Controls.Add(materialLabel1);
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(512, 64);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(509, 701);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(3, 64);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(509, 701);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = Properties.Resources.user_profile_icon_png_download_fa_user_circle_removebg_preview__1_;
+            panel3.Location = new Point(157, 21);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(195, 205);
+            panel3.TabIndex = 0;
+            // 
+            // materialLabel1
+            // 
+            materialLabel1.AutoSize = true;
+            materialLabel1.Depth = 0;
+            materialLabel1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel1.Location = new Point(60, 262);
+            materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel1.Name = "materialLabel1";
+            materialLabel1.Size = new Size(53, 19);
+            materialLabel1.TabIndex = 1;
+            materialLabel1.Text = "Email : ";
+            // materialMultiLineTextBox1
+            // 
+            materialMultiLineTextBox1.BackColor = Color.FromArgb(255, 255, 255);
+            materialMultiLineTextBox1.BorderStyle = BorderStyle.None;
+            materialMultiLineTextBox1.Depth = 0;
+            materialMultiLineTextBox1.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialMultiLineTextBox1.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialMultiLineTextBox1.Location = new Point(60, 304);
+            materialMultiLineTextBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialMultiLineTextBox1.Name = "materialMultiLineTextBox1";
+            materialMultiLineTextBox1.Size = new Size(389, 36);
+            materialMultiLineTextBox1.TabIndex = 2;
+            materialMultiLineTextBox1.Text = "";
+            // 
+            // materialMultiLineTextBox2
+            // 
+            materialMultiLineTextBox2.BackColor = Color.FromArgb(255, 255, 255);
+            materialMultiLineTextBox2.BorderStyle = BorderStyle.None;
+            materialMultiLineTextBox2.Depth = 0;
+            materialMultiLineTextBox2.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialMultiLineTextBox2.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialMultiLineTextBox2.Location = new Point(60, 413);
+            materialMultiLineTextBox2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialMultiLineTextBox2.Name = "materialMultiLineTextBox2";
+            materialMultiLineTextBox2.Size = new Size(389, 36);
+            materialMultiLineTextBox2.TabIndex = 4;
+            materialMultiLineTextBox2.Text = "";
+            // 
+            // materialLabel2
+            // 
+            materialLabel2.AutoSize = true;
+            materialLabel2.Depth = 0;
+            materialLabel2.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel2.Location = new Point(60, 371);
+            materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel2.Name = "materialLabel2";
+            materialLabel2.Size = new Size(83, 19);
+            materialLabel2.TabIndex = 3;
+            materialLabel2.Text = "Password : ";
+            // 
+            // materialButton1
+            // 
+            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton1.Depth = 0;
+            materialButton1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            materialButton1.HighEmphasis = true;
+            materialButton1.Icon = null;
+            materialButton1.Location = new Point(223, 481);
+            materialButton1.Margin = new Padding(4, 6, 4, 6);
+            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton1.Name = "materialButton1";
+            materialButton1.NoAccentTextColor = Color.Empty;
+            materialButton1.Size = new Size(64, 36);
+            materialButton1.TabIndex = 5;
+            materialButton1.Text = "Login";
+            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton1.UseAccentColor = false;
+            materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 768);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "LOGIN";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialMultiLineTextBox materialMultiLineTextBox2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialMultiLineTextBox materialMultiLineTextBox1;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+    }
+}
